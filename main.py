6a600b1e1c6954c2df9e23ba1bc0cab0d0b092c2
toms3776c <24806125+toms3776c@users.noreply.github.com>
2023-02-05 23:47:28 +0900

    Excelファイルのダウンロードを試す

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import StreamingResponse
+
+import openpyxl
+import pprint
 
 app = FastAPI()
 
@@ -18,3 +22,20 @@ app.add_middleware(
 @app.get("/")
 def Hello():
     return {"Hello":"World!"}
+
+
+@app.post("/export-excel!")
+def export_excel1():
+    filename = "test.xlsx"
+    wb = openpyxl.Workbook()
+    ws = wb.active
+    ws.cell(1,1).value = "テスト"
+    wb.save(filename)
+    wb.close()
+    wb = open(filename, "rb")
+    
+    #MIMEタイプを設定
+    XLSX_MIMETYPE = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+
+    headers = {"Content-Disposition": "attachment; filename=" + filename}
+    return StreamingResponse(content=wb, media_type=XLSX_MIMETYPE, headers=headers)
